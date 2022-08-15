963432cfaccca8818c6e8b177b1026fe06d86944
Auston Moos <AustonHMoos2004@gmail.com>
2022-08-15 11:06:17 -0700

    add more code to java

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,21 @@ secondh1.after(paragraph);
 
 const img = document.createElement("img");
 img.setAttribute("src", "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/202984001/900");
-img.setAttribute
-img.setAttribute
+img.setAttribute("alt", "This is an image of a Multicolored Tanager bird from birdsoftheworld.org");
+img.setAttribute("style", "width:50%");
+paragraph.after(img);
+
+const thirdh1 = document.createElement("h1");
+thirdh1.innerHTML = "Facts about the Multicolored Tanager";
+img.after(thirdh1);
+
+const ul = document.createElement("ul");
+thirdh1.after(ul);
+
+const li1 = document.createElement("li");
+li1.innerHTML = "It is endemic to the mountains of Colombia."
+const li2 = document.createElement("li");
+li2.innerHTML = "It usually searches for insects on the underside of leaves of outer limbs while clinging to leaves with its feet."
+
+ul.prepend(li1);
+ul.append(li2);
